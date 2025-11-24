2fc957cae5cd2316e643ade70018b0de0dc5c04d
Francisco Ferraz <2230706@my.ipleiria.pt>
2025-11-24 17:10:23 +0000

    Add CORS filter and custom exception handling for REST API

---
+++ src/main/java/pt/ipleiria/dei/ei/estg/researchcenter/exceptions/MyConstraintViolationException.java
@@ -0,0 +1,18 @@
+package pt.ipleiria.dei.ei.estg.researchcenter.exceptions;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import java.util.stream.Collectors;
+
+public class MyConstraintViolationException extends Exception {
+    public MyConstraintViolationException(ConstraintViolationException e) {
+        super(getConstraintViolationMessages(e));
+    }
+
+    private static String getConstraintViolationMessages(ConstraintViolationException e) {
+        return e.getConstraintViolations()
+                .stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining("; "));
+    }
+}
